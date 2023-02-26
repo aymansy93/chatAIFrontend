875b6b9caaca496ee10f11737d8c169babe59781
aymansy93 <ayman_kan94@protonmail.com>
2023-02-26 13:41:24 +0200

    first commit

---
+++ src/component/content/Content.js
@@ -0,0 +1,79 @@
+import React from 'react'
+import { useEffect, useState } from 'react'
+import './content.css'
+
+
+
+
+const Content = () => {
+
+    const [question, Setquestion] = useState('');
+    const [answer, Setanswer] = useState('');
+
+
+    const handelSubmit = (e) => {
+        e.preventDefault();
+        Setquestion(e.target.elements.question.value);
+
+        // console.log(answer);
+    }
+
+    // useEffect(()=> {},[]);
+
+
+    useEffect(() => {
+        const fetchAnswer = async () => {
+            try {
+                const response = await fetch(`http://127.0.0.1/api/ask/${question}`);
+                const data = await response.json();
+
+                if (response.ok) {
+                    Setanswer(data.text);
+                } else {
+                    throw new Error(data.error);
+                }
+            }
+            catch (error) {
+                console.error(error.message);
+            }
+        };
+
+        if (question) {
+            fetchAnswer();
+            Setquestion('');
+        }
+    }, [question]);
+
+
+
+
+
+
+    return (
+        <div className='container'>
+            <form onSubmit={handelSubmit}>
+                <div className="form-group">
+                    <label for="exampleFormControlInput1">Question</label>
+                    <input type='text' className="form-control"
+                        id="exampleFormControlInput1" name='question' placeholder="type question arabic or english" />
+                </div>
+                <div className="form-group row mt-5">
+                    <div className="col-sm-10 d-flex justify-content-center">
+                        <button type="submit" className="btn btn-primary">Ask Me</button>
+                    </div>
+                </div>
+            </form>
+
+            <div className="answer-container">
+                {answer && (
+                    <div className="answer">
+                        <p className="answer-title">Answer</p>
+                        <p className="answer-text">{answer}</p>
+                    </div>
+                )}
+            </div>
+        </div>
+    )
+}
+
+export default Content
---
+++ src/component/header/Header.js
@@ -0,0 +1,13 @@
+import './header.css'
+
+const Header = () => {
+  return (
+    <nav className="navbar  navbar-expand-md bg-primary text-light">
+        <div className="container d-flex justify-content-center">
+            <span className="title">مرحبا بك في تطبيق اسالني واجيب</span>
+        </div>
+    </nav>
+  )
+}
+
+export default Header
